cf72b6dc6a5b645e748a75b01db3870944af9131
saurabh.vaj <saurabh.vaj>
2017-01-09 23:10:43 +0530

    added files

--- code/config.php
+++ code/config.php
@@ -19,6 +19,6 @@ $databaseName = 'test';
 $databaseUsername = 'root';
 $databasePassword = 'root';
 
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName) or die(mysql_error($mysqli); 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
  
 ?>

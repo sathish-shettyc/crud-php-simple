bb77eb5733addbf104c5315d057ed70232a16835
saurabh.vaj <saurabh.vaj>
2017-01-10 18:13:05 +0530

    updated readme.md

--- application/code/database.sql
+++ application/code/database.sql
@@ -1,8 +1,8 @@
-create database test;
+create database test IF NOT EXIST;
 
 use test;
 
-CREATE TABLE `users` (
+CREATE TABLE `test.users` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(100) NOT NULL,
   `age` int(3) NOT NULL,

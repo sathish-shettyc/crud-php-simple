f4c87fc9b9d8819feb3337c313655117f1b3272a
saurabh.vaj <saurabh.vaj>
2017-01-10 11:55:48 +0530

    added pipline steps

---
+++ jenkins/pipelinesteps/CodeDeploymentPipelineSteps.groovy
@@ -0,0 +1,14 @@
+stage 'CodeStability'
+    build 'CodeStability'
+    
+stage 'CodeQuality'
+    build 'CodeQuality'
+
+stage 'CodeCoverage'
+    build 'CodeCoverage'
+
+stage 'ImageGenerator'
+    build 'ImageGenerator'
+
+stage 'CodeDeployer'
+    build 'CodeDeployer'
